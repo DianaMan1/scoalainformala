37069d162a7bc37bde2f4bdff7afc5f167a3308f
unknown <dianacerlinca95@gmail.com>
2022-01-20 17:12:08 +0200

    mesaj

---
+++ data_structure.py
@@ -0,0 +1,17 @@
+#lista = [8, 2, -5, "string", 2.7]
+#print(lista)
+#print(lista[3])
+#print(lista[5]) # dacă indexul nu există primim o eroare
+#print(lista[5:1]) # nu apare eroare
+#print(lista[0:5:2]) # afișează elementele de la index 0 până la index 5(exclus) din 2 în 2
+#print(lista[-1]) # afișează ultimul element din listă
+#print(lista[-1: :-1]) # afișează în mod descrescător poziția fiecărui element din listă
+#lista.append("mere") # adaugă la sfârșit elementul în listă
+#lista[3] = "mere" #înlocuiește elementul care are indexul 3 din listă
+#lista.insert(3, "element nou") # adaugă un element nou în indexul specificat decalând lista
+#print(len(lista)) # ne afișează numărul elementelor din listă
+#print(lista[2]) # ne afișează elementul din listă care are indexul 2
+#lista.remove(-5) # elimină valoarea specificată
+#lista.pop(2) # elimină valoarea de pe indexul specificat, adică -5
+#lista.clear() # elimină toate elementele din listă
+#lista.reverse() # inversează elementele din listă
